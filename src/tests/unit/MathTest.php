125073f83a1b32874b1b01bcb32654d6f2bebe46
tanguybernard <bernardtanguy1pro@gmail.com>
2022-01-21 10:15:01 +0100

    add math test 10

--- src/tests/unit/MathTest.php
+++ src/tests/unit/MathTest.php
@@ -8,6 +8,7 @@ final class MathTest extends TestCase
     public function testAddition(){
         $this->assertEquals(3, $this->addition(1, 2));
         $this->assertEquals(4, $this->addition(2, 2));
+        $this->assertEquals(10, $this->addition(6, 4));
     }
 
     private function addition(int $int, int $int1): int
